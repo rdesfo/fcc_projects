28eb08b71c67c2d33b4feb690acfc3d882d58625
Ryan Desfosses <ryan@desfo.org>
2016-08-24 03:36:13 +0000

    onclick gets text from input field

--- WikipediaViewer/WikipediaViewer.js
+++ WikipediaViewer/WikipediaViewer.js
@@ -1,24 +1,35 @@
-var wikiSearch = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + "batman" + "&callback=?";
 
+$(document).ready(function() {
+  
+  $('#btnSearch').on("click", function(){
+    var text= document.getElementById('search').value;
+    console.log(text);
+  
+    doSearch(text);
+  })
+  
+var doSearch = function(text){
+  var wikiSearch = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + text + "&callback=?";
 
-$.getJSON(wikiSearch,null,function(json){
-  var result = "";
+  $.getJSON(wikiSearch,null,function(json){
+    var result = "";
 
-  $.each(json.query.search, function(key, value){
-  //  console.log("key: " + JSON.stringify(key) + " and value: " + JSON.stringify(value));
-  //console.log(JSON.stringify(value.title));
-  //console.log(JSON.stringify(value.snippet));
+    $.each(json.query.search, function(key, value){
+    //console.log("key: " + JSON.stringify(key) + " and value: " + JSON.stringify(value));
+    //console.log(JSON.stringify(value.title));
+    //console.log(JSON.stringify(value.snippet));
 
-  result += "<div>";
-  result += "<h2>" + JSON.stringify(value.title) + "</h2>";
-  result += "<p>" + JSON.stringify(value.snippet) + "</p>";
-  result += "</div>";
+    result += "<div>";
+    result += "<h2>" + JSON.stringify(value.title) + "</h2>";
+    result += "<p>" + JSON.stringify(value.snippet) + "</p>";
+    result += "</div>";
 
-  // "https://en.wikipedia.org/wiki/" + encodeURI("some title")
-  });
+    // "https://en.wikipedia.org/wiki/" + encodeURI("some title")
+    });
 
-  $(".search").html(result);
-  console.log(result);
+    $(".results").html(result);
+    console.log(result);
 
-},'json');
-
+  },'json');
+};
+}); //ready
