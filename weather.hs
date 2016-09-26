c959b1a25f0b3f1b3cf3c1583b900c38f738ba5f
Ryan Desfosses <ryan@desfo.org>
2016-09-26 04:35:43 +0000

    passes lat and lon to weather request

--- weather.hs
+++ weather.hs
@@ -1,12 +1,30 @@
+
 import Haste
 import Haste.Ajax
+import Haste.JSON
+import qualified Haste.JSString as JS
 
-testUrl = "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=e0737aaec18774db13c1434bc50f2fe5"
+ipUrl :: JSString
+ipUrl = toJSString "http://ip-api.com/json"
 
+weatherUrl :: JSON -> JSString
+weatherUrl json  = JS.concat [ toJSString "http://api.openweathermap.org/data/2.5/weather?lat="
+                             , lat 
+		             , toJSString "&lon="
+		             , lon
+		             , toJSString "&APPID=e0737aaec18774db13c1434bc50f2fe5"
+		             ]
+    where lat = toJSString $ json ! (toJSString "lat")
+          lon = toJSString $ json ! (toJSString "lon")
+
+
+main :: IO ()
 main = do 
-  test <- fmap toJSString $ ajaxRequest GET (toJSString testUrl) noParams $ \c' ->
-    case c' of
-      Nothing -> return ()
-      Just c -> writeLog c
+  ajaxRequest GET ipUrl noParams 
+    $ \callback -> case callback of
+  	Nothing -> return ()
+        Just a  -> ajaxRequest GET (weatherUrl a) noParams $
+		       \c' -> case c' of
+				Nothing -> return()
+				Just c  -> writeLog c
   return ()
-
