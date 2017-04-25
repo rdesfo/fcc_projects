8a0d04ee8ba1bcf106f3ec86850c4eaf81e1b08f
Ryan Desfosses <ryan@desfo.org>
2017-04-25 13:48:55 -0400

    rearranged code and added units=imperial to api request

--- LocalWeatherProject/localweather.js
+++ LocalWeatherProject/localweather.js
@@ -1,18 +1,27 @@
-var $ = require("jquery");
-$.getScript("http://ip-api.com/json/?callback=getIP")
+$(document).ready(function(){
 
-function getIP(data) {
-  var myLat = Math.round(data.lat);
-  var myLon = Math.round(data.lon);
-      
-  console.log(data);
-  console.log("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e0737aaec18774db13c1434bc50f2fe5");
-          
-  $.get("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e0737aaec18774db13c1434bc50f2fe5", function(data, textStatus, jqxhr) {
-    console.log(data); // Data returned
-    console.log(textStatus); // Success
-    console.log(jqxhr.status); // 200
-                            
-    $(".message").html(JSON.stringify(data.weather[0].main));
-  });
-}
+ function getIP(data) {
+     var myLat = Math.round(data.lat);
+     var myLon = Math.round(data.lon);
+     var result = "";
+           
+    $.get("http://api.openweathermap.org/data/2.5/weather?lat=" + myLat + "&lon=" + myLon + "&units=imperial&appid=e0737aaec18774db13c1434bc50f2fe5", function(data, textStatus, jqxhr) {
+      console.log( JSON.stringify( data )); // Data returned
+      console.log(textStatus); // Success
+      console.log(jqxhr.status); // 200
+
+	result += '<p>' + JSON.stringify(data.weather[0].main) + '</p>';
+	result += '<p>' + JSON.stringify(data.main.temp) + '</p>';
+                           
+      $(".message").html(result);
+    });
+ }
+
+
+ $.getJSON( "http://ip-api.com/json/", function( data, textStatus, jqxhr ) {
+  console.log( JSON.stringify( data )); // Data returned
+  console.log( textStatus ); // Success
+  console.log( jqxhr.status ); // 200
+ }).done( getIP );
+
+ });
