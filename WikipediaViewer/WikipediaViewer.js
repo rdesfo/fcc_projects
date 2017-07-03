f587a2558b2b7f933fc6cc619a3ca222f2fadde3
Ryan Desfosses <ryan@desfo.org>
2017-07-03 09:47:48 -0400

    wikipediaviewer: adde proper spacing for center function

--- WikipediaViewer/WikipediaViewer.js
+++ WikipediaViewer/WikipediaViewer.js
@@ -2,14 +2,14 @@
 
 $(document).ready(function() {
 
+  jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("bottom", "50%");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+      $(window).scrollLeft()) + "px");
+    return this;
+  }
 
-jQuery.fn.center = function () {
-  this.css("position","absolute");
-  this.css("bottom", "50%");
-  this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
-    $(window).scrollLeft()) + "px");
-  return this;
-}
 
   $('.text-center').center();
   
