fca6a0739027ec72464f971389e5a2052506e2a2
Ryan Desfosses <ryan@desfo.org>
2017-07-11 12:30:42 -0400

    shows condition and temp

--- LocalWeather/localweather.js
+++ LocalWeather/localweather.js
@@ -37,9 +37,8 @@ $(document).ready(function(){
 
   function updateResult(data){
     var result = "";
-    result += '<p><i class="owf owf-5x owf-' + JSON.stringify(data.weather[0].id) + '"></i></p>';
-    result += '<p>' + JSON.stringify(data.weather[0].main) + '</p>';
-    result += '<p>' + JSON.stringify(Math.round(data.main.temp)) + 'F</p>';
+    result += '<p>' + data.current.condition.text + '</p>';
+    result += '<p>' + data.current.temp_f + '</p>';
     $(".message").html(result);
   }
 });
