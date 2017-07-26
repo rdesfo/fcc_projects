13ac7fac82e7646fe609b05af833214a71388902
Ryan Desfosses <ryan@desfo.org>
2017-07-26 12:54:11 -0400

    removed center jquery function

--- LocalWeather/localweather.js
+++ LocalWeather/localweather.js
@@ -1,15 +1,5 @@
 $(document).ready(function(){
 
-  jQuery.fn.center = function () {
-    this.css("position","absolute");
-    this.css("bottom", "50%");
-    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
-      $(window).scrollLeft()) + "px");
-    return this;
-  }
-
-  $('.container').center();
-
   navigator.geolocation.getCurrentPosition(myPosition);
 
   function myPosition(position) {
